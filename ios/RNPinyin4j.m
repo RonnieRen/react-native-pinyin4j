025d33e848ae9f86ca85185ab0a3f1e4e4f7eef2
Ronnie Ren <renzhaosong@niwodai.net>
2017-11-07 17:45:44 +0800

    for ios, if passing in a empty or nil string, throw a error

--- ios/RNPinyin4j.m
+++ ios/RNPinyin4j.m
@@ -1,6 +1,10 @@
 
 #import "RNPinyin4j.h"
 
+#define REJECTORWITHERRORMSG(rejector, errorMsg)  NSError* er =  [NSError errorWithDomain: @"com.geerong.www" code:10000 userInfo:@{@"description": @#errorMsg}]; \
+rejector([NSString stringWithFormat:@"%ld", er.code], er.localizedDescription, er);
+
+
 @implementation RNPinyin4j
 
 - (dispatch_queue_t)methodQueue
@@ -12,11 +16,14 @@ RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(getHanyuPinyinStringArray: (NSString*)firstCharacter resolver: (RCTPromiseResolveBlock)resolver
                   rejecter:(RCTPromiseRejectBlock)reject ) {
-    
+    if(!firstCharacter){
+        REJECTORWITHERRORMSG(reject, "nil value is not supported");
+        return;
+    }
     NSMutableString* str = [NSMutableString stringWithString:firstCharacter];
     CFStringTransform((CFMutableStringRef)str, NULL, kCFStringTransformToLatin, false);
     CFStringTransform((CFMutableStringRef)str, NULL, kCFStringTransformStripDiacritics, NO);
     resolver(@[str]);
 }
 @end
-  
+
